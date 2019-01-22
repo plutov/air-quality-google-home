8ff346b22f912988997c48eff2e9b0fd1be7a26f
Alex Pliutau <a.pliutau@gmail.com>
2019-01-22 14:15:12 +0100

    basic auth

--- app.go
+++ app.go
@@ -18,6 +18,21 @@ var (
 )
 
 func handle(w http.ResponseWriter, r *http.Request) {
+	/*auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(auth) != 2 || auth[0] != "Basic" {
+		http.Error(w, "authorization failed", http.StatusUnauthorized)
+		return
+	}
+
+	payload, _ := base64.StdEncoding.DecodeString(auth[1])
+	pair := strings.SplitN(string(payload), ":", 2)
+
+	if len(pair) != 2 || pair[0] != os.Getenv("AUTH_USER") || pair[1] != os.Getenv("AUTH_PASS") {
+		http.Error(w, "authorization failed", http.StatusUnauthorized)
+		return
+	}
+	*/
+
 	dfReq := DialogFlowRequest{}
 	dfErr := json.NewDecoder(r.Body).Decode(&dfReq)
 
